3718e482c70d48d0cc23ef7f33a68ae75461ca51
Buezrello <igindin@gmail.com>
2023-05-26 19:06:07 +0300

    reverse linked list

---
+++ solutions/ReverseLinkedList.go
@@ -0,0 +1,20 @@
+package solutions
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func reverseList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	temp := reverseList(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+
+	return temp
+}
